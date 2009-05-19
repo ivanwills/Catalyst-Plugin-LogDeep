eff5c102bb6e6b137561777465f71e07b68d5d02
Ivan Wills <ivan.wills@gmail.com>
2009-05-20 04:45:25 +1000

    Initial import

---
+++ lib/Catalyst/Plugin/LogDeep.pm
@@ -0,0 +1,173 @@
+package Catalyst::Plugin::LogDeep;
+
+# Created on: 2009-05-20 04:09:42
+# Create by:  ivan
+# $Id$
+# $Revision$, $HeadURL$, $Date$
+# $Revision$, $Source$, $Date$
+
+use strict;
+use warnings;
+use version;
+use Carp;
+use List::Util qw/ first /;
+use Log::Deep;
+use English qw/ -no_match_vars /;
+use base qw/Exporter/;
+
+our $VERSION     = version->new('0.0.1');
+
+my $first = 1;
+sub setup {
+	my $package = shift;
+	my $pkgname = ref $package || $package;
+
+	if ($first) {
+		$first = 0;
+
+		do {
+			no strict 'refs';
+			@{"${pkgname}::ISA"} = grep { $_ ne __PACKAGE__ } @{"${pkgname}::ISA"};
+		};
+
+		#my $confname = lc $package;
+		#$confname =~ s/::/_/g;
+
+		#my $logpath = first { -s $_ }
+		#	( "${confname}_log.conf", "log.conf",
+		#	"../${confname}_log.conf", "../log.conf",
+		#	"/etc/${confname}_log.conf", "/etc/$confname/log.conf" );
+
+		#if (defined $logpath) {
+		#	$package->log(Catalyst::Log::Log4perl->new($logpath));
+		#}
+		#else {
+		#	$package->log(Catalyst::Log::Log4perl->new());
+		#	$package->log->warn('no log4perl configuration found');
+		#}
+		my $cfg = $package->config->{'Plugin::LogDeep'} || {};
+
+		$package->log( Log::Deep->new( %{ $cfg } ) );
+		$package->log->enable('debug');
+		$package->log->debug("How do I set the level?");
+	}
+
+	$package->NEXT::setup(@_);
+};
+
+1;
+
+__END__
+
+=head1 NAME
+
+Catalyst::Plugin::LogDeep - <One-line description of module's purpose>
+
+=head1 VERSION
+
+This documentation refers to Catalyst::Plugin::LogDeep version 0.1.
+
+
+=head1 SYNOPSIS
+
+   use Catalyst::Plugin::LogDeep;
+
+   # Brief but working code example(s) here showing the most common usage(s)
+   # This section will be as far as many users bother reading, so make it as
+   # educational and exemplary as possible.
+
+
+=head1 DESCRIPTION
+
+A full description of the module and its features.
+
+May include numerous subsections (i.e., =head2, =head3, etc.).
+
+
+=head1 SUBROUTINES/METHODS
+
+A separate section listing the public components of the module's interface.
+
+These normally consist of either subroutines that may be exported, or methods
+that may be called on objects belonging to the classes that the module
+provides.
+
+Name the section accordingly.
+
+In an object-oriented module, this section should begin with a sentence (of the
+form "An object of this class represents ...") to give the reader a high-level
+context to help them understand the methods that are subsequently described.
+
+
+=head3 C<setup ( $search, )>
+
+Param: C<$search> - type (detail) - description
+
+Return: Catalyst::Plugin::LogDeep -
+
+Description:
+
+=cut
+
+=head1 DIAGNOSTICS
+
+A list of every error and warning message that the module can generate (even
+the ones that will "never happen"), with a full explanation of each problem,
+one or more likely causes, and any suggested remedies.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+A full explanation of any configuration system(s) used by the module, including
+the names and locations of any configuration files, and the meaning of any
+environment variables or properties that can be set. These descriptions must
+also include details of any configuration language used.
+
+=head1 DEPENDENCIES
+
+A list of all of the other modules that this module relies upon, including any
+restrictions on versions, and an indication of whether these required modules
+are part of the standard Perl distribution, part of the module's distribution,
+or must be installed separately.
+
+=head1 INCOMPATIBILITIES
+
+A list of any modules that this module cannot be used in conjunction with.
+This may be due to name conflicts in the interface, or competition for system
+or program resources, or due to internal limitations of Perl (for example, many
+modules that use source code filters are mutually incompatible).
+
+=head1 BUGS AND LIMITATIONS
+
+A list of known problems with the module, together with some indication of
+whether they are likely to be fixed in an upcoming release.
+
+Also, a list of restrictions on the features the module does provide: data types
+that cannot be handled, performance issues and the circumstances in which they
+may arise, practical limitations on the size of data sets, special cases that
+are not (yet) handled, etc.
+
+The initial template usually just has:
+
+There are no known bugs in this module.
+
+Please report problems to ivan (ivan@localhost).
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+ivan - (ivan@localhost)
+<Author name(s)>  (<contact address>)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009 ivan (123 Timbuc Too).
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+=cut
