558b673b98a7fe2d61b5982dc723138b2ae1bd40
Ivan Wills <ivan.wills@gmail.com>
2009-05-20 05:44:53 +1000

    Added missing tests, fixed their errors. Added example code

--- lib/Catalyst/Plugin/LogDeep.pm
+++ lib/Catalyst/Plugin/LogDeep.pm
@@ -1,7 +1,7 @@
 package Catalyst::Plugin::LogDeep;
 
 # Created on: 2009-05-20 04:09:42
-# Create by:  ivan
+# Create by:  Ivan Wills
 # $Id$
 # $Revision$, $HeadURL$, $Date$
 # $Revision$, $Source$, $Date$
@@ -61,107 +61,70 @@ __END__
 
 =head1 NAME
 
-Catalyst::Plugin::LogDeep - <One-line description of module's purpose>
+Catalyst::Plugin::LogDeep - Sets up L<Log::Deep> for Catalyst logging
 
 =head1 VERSION
 
 This documentation refers to Catalyst::Plugin::LogDeep version 0.1.
 
-
 =head1 SYNOPSIS
 
-   use Catalyst::Plugin::LogDeep;
+ use Catalyst qw/ ... LogDeep/;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
+ __PACKAGE__->config(
+     'Plugin::LogDeep' => {
+         -name  => __PACKAGE__,
+         -level => [ qw/debug warn error fatal/ ],
+    },
+ );
 
+ $c->log->debug( { var => $variable }, 'This is the value of variable );
+ $c->log->error( 'You did not do something' );
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
+Allows Catalyst to use the L<Log::Deep> library for logging operations.
 
-May include numerous subsections (i.e., =head2, =head3, etc.).
+The values set in the Plugin::LogDeep configuration item are passed directly
+on to the L<Log::Deep> new method so look there for all the options for
+configuration.
 
+Note: You currently need to tell add a call to $c->log->session in [every]
+begin method if you want a per session log session id. Hopefully this wont
+be required in future versions.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=head3 C<setup ()>
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-=head3 C<setup ( $search, )>
-
-Param: C<$search> - type (detail) - description
-
-Return: Catalyst::Plugin::LogDeep -
-
-Description:
-
-=cut
+Description: Sets up the catalyst application to use L<Log::Deep> as it's log
+object.
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
+This module has only two dependencies C<Log::Deep> and L<Catalyst>
 
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
-Please report problems to ivan (ivan@localhost).
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
 
 Patches are welcome.
 
 =head1 AUTHOR
 
-ivan - (ivan@localhost)
-<Author name(s)>  (<contact address>)
+Ivan Wills (ivan.wills@gmail.com)
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2009 ivan (123 Timbuc Too).
+Copyright (c) 2009 Ivan Wills (14 Mullion Close Hornsby Heights, NSW, Australia 2077).
 All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it under
